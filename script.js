f7a68957f3553372b7924b549e983b3b6f3fe00e
JCXZ07 <johncarlobugarin37@gmail.com>
2025-02-06 14:25:02 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 const messages = [
-    "Are you sure?",
-    "Really sure??",
-    "Are you positive?",
-    "Pookie please...",
-    "Just think about it!",
-    "If you say no, I will be really sad...",
-    "I will be very sad...",
-    "I will be very very very sad...",
+    "Sure ka?",
+    "Ehhhh",
+    "Mali ka ata ng pinindot",
+    "Ehh bilhan kita melkti",
+    "<--Yes For Samgyup!!",
+    "GALIT MO KO!!",
+    "Joke lang yes ka na hehe",
+    "Date tayo kahit saan mo gusto!!!",
     "Ok fine, I will stop asking...",
     "Just kidding, say yes please! ❤️"
 ];
